74893d7531ebd435739e307cc48e13799fde797f
Vishnu Ks <hackerkid@vishnuks.com>
2016-06-30 04:17:19 +0530

    add oh-my-zsh

--- first-time.sh
+++ first-time.sh
@@ -2,6 +2,7 @@ sudo apt-get install terminator
 sudo apt-get install vim
 sudo apt-get install zsh
 sudo apt-get install git
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 
 
